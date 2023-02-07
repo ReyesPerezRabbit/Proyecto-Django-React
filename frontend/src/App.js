e202c0e5043ec8d3a11634d830dacedd89e9360c
Saade1 <203040@uptapachula.edu.mx>
2023-02-07 00:57:11 -0600

    algo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,76 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
+import Inicio from "./components/inicio";
+import LoginAdmin from "./components/admin/login_Admin";
+import LoginUssers from "./components/ussers/login_Ussers";
+import SolicitudLibros from "./components/ussers/solicitudLibros";
+//import axios from 'axios';
 
-// Components
-import Header from "./components/Header";
-import BlogForm from "./components/BlogForm";
-import Blogs from "./components/Blogs";
+// // Components
+// import Header from "./components/Header";
+// import BlogForm from "./components/BlogForm";
+// import Blogs from "./components/Blogs";
 
 
 function App() {
-  <h1>Hola</h1>
- 
-  const [blogs, setBlogs] = useState([]);
-  
-  useEffect(() => {
-    axios.get('/get/')
-    .then((response) => {
-      setBlogs(response.data)
-    }).catch(() => {
-      alert('Algo fue mal!')
-    })
-  }, [])
-
   return (
-    <>
-    <Header/>
-    <div className="container p-4">
-     <BlogForm blogs={blogs} setBlogs={setBlogs}/>
-     <Blogs blogs={blogs} setBlogs={setBlogs}/>
-     
-    </div>
-    
-    </>
-  );
+    <Router>
+      <div className="container">
+        <div className="btn-group">
+          <NavLink to="/" className="btn btn-dark">
+            inicio
+          </NavLink>
+          <NavLink to="/admin" className="btn btn-dark">
+            admin
+          </NavLink>
+          <NavLink to="/ussers" className="btn btn-dark">
+            usuarios
+          </NavLink>
+          <NavLink to="/solicitud" className="btn btn-dark">
+            solicitud
+          </NavLink>
+        </div>
+        <hr />
+        <Switch>
+          <Route path="/" exact>
+            <Inicio />
+          </Route>
+          <Route path="/admin" exact>
+            <LoginAdmin />
+          </Route>
+          <Route path="/ussers" exact>
+            <LoginUssers />
+          </Route>
+          <Route path="/solicitud" exact>
+            <SolicitudLibros />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+    // const [blogs, setBlogs] = useState([]);
+
+    // useEffect(() => {
+    //   axios.get('/get/')
+    //   .then((response) => {
+    //     setBlogs(response.data)
+    //   }).catch(() => {
+    //     alert('Algo fue mal!')
+    //   })
+    // }, [])
+
+    // return (
+
+    //   <>
+    //   <Header/>
+    //   <div className="container p-4">
+    //    <BlogForm blogs={blogs} setBlogs={setBlogs}/>
+    //    <Blogs blogs={blogs} setBlogs={setBlogs}/>
+    //    {/* <h1>Hola mundo :O</h1> */}
+
+    //   </div>
+
+    //   </>
+  )
 }
+
 export default App;
---
+++ frontend/src/components/admin/login_Admin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const login_Admin = () => {
+    return (
+      <div>
+          <h1>Login Admin</h1>
+      </div>
+    )
+  }
+  
+export default login_Admin
