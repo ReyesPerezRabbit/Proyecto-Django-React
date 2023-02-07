20e2b34fc3c3139737993f7e1379a9d9ff617f6b
ReyesPerezRabbit <203424@uptapachula.edu.mx>
2023-02-07 00:55:21 -0600

    nose

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,37 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 // Components
 import Header from "./components/Header";
 import BlogForm from "./components/BlogForm";
 import Blogs from "./components/Blogs";
-//import Login from "./usuarios/login";
-
 
 function App() {
- 
   const [blogs, setBlogs] = useState([]);
-  
+
   useEffect(() => {
-    axios.get('/get/')
-    .then((response) => {
-      setBlogs(response.data)
-    }).catch(() => {
-      alert('Algo fue mal!')
-    })
-  }, [])
+    axios
+      .get("/get/")
+      .then((response) => {
+        setBlogs(response.data);
+      })
+      .catch(() => {
+        alert("Algo fue mal!");
+      });
+  }, []);
 
   return (
     <>
-    <Header/>
-    <div className="container p-4">
-     <BlogForm blogs={blogs} setBlogs={setBlogs}/>
-     <Blogs blogs={blogs} setBlogs={setBlogs}/>
-     
-    </div>
-    <div>
-      <h1>hola</h1>
-    </div>
+      <Header />
+
+      <div className="container p-4">
+        <BlogForm blogs={blogs} setBlogs={setBlogs} />
+        <Blogs blogs={blogs} setBlogs={setBlogs} />
+      </div>
+      <div>
+        <h1>hola</h1>
+      </div>
     </>
   );
-  
-  
 }
 export default App;
